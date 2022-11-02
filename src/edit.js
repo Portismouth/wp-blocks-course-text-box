fc5e653313216451659ff7f98e064ac818cc46b6
Ozzy Gonzalez <ozzy.gonzalez@onesixsolutions.com>
2022-11-02 10:26:41 -0700

    color palletes - about half done with module

--- src/edit.js
+++ src/edit.js
@@ -1,11 +1,76 @@
 import { __ } from '@wordpress/i18n';
-import { useBlockProps } from '@wordpress/block-editor';
+import {
+	useBlockProps,
+	RichText,
+	BlockControls,
+	AlignmentToolbar,
+	InspectorControls,
+	PanelColorSettings,
+	ContrastChecker,
+} from '@wordpress/block-editor';
+import {} from '@wordpress/components';
 import './editor.scss';
 
-export default function Edit() {
+export default function Edit( { attributes, setAttributes } ) {
+	const { text, alignment, backgroundColor, textColor } = attributes;
+	const onChangeText = ( newText ) => {
+		setAttributes( { text: newText } );
+	};
+	const onChangeAlignment = ( newAlignment ) => {
+		setAttributes( { alignment: newAlignment } );
+	};
+	const onBackgroundColorChange = ( newBgColor ) => {
+		setAttributes( { backgroundColor: newBgColor } );
+	};
+	const onTextColorChange = ( newTextColor ) => {
+		setAttributes( { textColor: newTextColor } );
+	};
 	return (
-		<p { ...useBlockProps() }>
-			{ __( 'Boilerplate – hello from the editor!', 'boilerplate' ) }
-		</p>
+		<>
+			<InspectorControls>
+				<PanelColorSettings
+					title={ __( 'Color Settings', 'text-box' ) }
+					icon="admin-appearance"
+					initialOpen
+					colorSettings={ [
+						{
+							value: backgroundColor,
+							onChange: onBackgroundColorChange,
+							label: __( 'Background Color', 'text-box' ),
+						},
+						{
+							value: textColor,
+							onChange: onTextColorChange,
+							label: __( 'Text Color', 'text-box' ),
+						},
+					] }
+				>
+					<ContrastChecker
+						textColor={ textColor }
+						backgroundColor={ backgroundColor }
+					/>
+				</PanelColorSettings>
+			</InspectorControls>
+			<BlockControls>
+				<AlignmentToolbar
+					onChange={ onChangeAlignment }
+					value={ alignment }
+				/>
+			</BlockControls>
+			<RichText
+				{ ...useBlockProps( {
+					className: `text-box-align-${ alignment }`,
+					style: {
+						backgroundColor,
+						color: textColor,
+					},
+				} ) }
+				onChange={ onChangeText }
+				value={ text }
+				placeholder={ __( 'Your Text', 'text-box' ) }
+				tagName="h4"
+				allowedFormats={ [ 'core/bold' ] }
+			/>
+		</>
 	);
 }
